5117ba797bb5e9e49ffac7241e4cf717cdf82f94
LucasXXI <lucasleal2001@gmail.com>
2023-05-10 21:32:04 -0300

    feat: adding userId query param.

--- serverless.ts
+++ serverless.ts
@@ -73,7 +73,7 @@ const serverlessConfiguration = {
       events: [
         {
           http: {
-            path: 'category',
+            path: 'category/{userId}}',
             method: 'get',
           },
         },
